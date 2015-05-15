715f200e46ae0e4ce57c988ba80d35426ddd3da3
faroncoder <faronledger@gmail.com>
2015-05-15 13:14:09 -0400

    new scripts

--- video/thumbnailing/thumbthis.sh
+++ video/thumbnailing/thumbthis.sh
@@ -5,10 +5,10 @@
         mkdir -p $PWD/thumbs
 fi
 
-INPUT="$( find $PWD -maxdepth 1 -type f -name '*.mp4' -exec basename {} \; | sort | head -n 1)"
+INPUT="$( find $PWD -maxdepth 1 -type f -name '*.mkv' -exec basename {} \;  )"
 if [ -z "$1" ];
 	then
-		INPUT=( $( find $PWD -maxdepth 1 -type f -name '*.mp4' -exec basename {} \; ) )
+		INPUT=( $( find $PWD -maxdepth 1 -type f -name '*.mkv' -exec basename {} \; ) )
 	else
 		INPUT="$1"
 fi
@@ -18,13 +18,11 @@ if [ -z "$INPUT" ];
     	echo "nope there is no file -- ending the encoding engine..."
  		exit 0
 fi
-for f in "${INPUT[@]}"
-	do
+for f in "${INPUT[@]}"; do
 		HEIGHTPNG="256"
 
+		ffmpeg -ss 00:01:00 -i $f -y -t 1 -vf scale=-1:$HEIGHTPNG -f image2 -vframes 1 "$PWD/thumbs/$f.png" < /dev/null
 
-
-		ffmpeg -ss 00:01:00 -i $f -y -t 1 -vf scale=-1:$HEIGHTPNG -f image2 -vframes 1 "thumbs/$f.png" < /dev/null ;
 
 		#ffmpeg -ss 00:01:00 -i $f -y -t 1 -vf "scale=trunc(oh*a/2)*2:$HEIGHTPNG" -f image2 -vframes 1 "thumbs/$f.png" < /dev/null ;
 	done
