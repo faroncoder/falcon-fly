7f9397c7f0016922c791ff45284d7ea9ed658692
faroncoder <faronledger@gmail.com>
2015-05-20 13:49:16 -0400

    auto-committing via script

--- git/git-config.sh
+++ git/git-config.sh
@@ -6,7 +6,6 @@ if [[ -d "$PWD/.git" ]]; then
 	CHECKGIT=$( cat .git/config | wc -l )
 	if [[ "$CHECKGIT" == 16 ]]; then
 		echo "your git configuration is valid"
-		exit 0
 	else
 		echo "
 [core]
@@ -14,15 +13,15 @@ if [[ -d "$PWD/.git" ]]; then
 	filemode = true
 	bare = false
 	logallrefupdates = true
-[remote \/"origin\/"]
+[remote \"origin\"]
 	url = git@github.com:faroncoder/$GITNAME
 	fetch = +refs/heads/*:refs/remotes/origin/*
-[branch \/"master\/"]
+[branch \"master\"]
 	remote = origin
 	merge = refs/heads/master
 [user]
 	name = faroncoder
-[remote \/"$GITNAME\/"]
+[remote \"$GITNAME\"]
 	url = git@github:/faroncoder/$GITNAME
 	fetch = +refs/heads/*:refs/remotes/$GITNAME/*
 " > $PWD/.git/config
