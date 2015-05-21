173d44125faaf847ee37377d7a7ca95f2fa1c894
faroncoder <faronledger@gmail.com>
2015-05-20 23:11:11 -0400

    auto-committing via script

--- git/git-auto.sh
+++ git/git-auto.sh
@@ -6,6 +6,7 @@
 find /home/faron/.falcon/scripting -maxdepth 3 -type d -name '.git' > ~/x.txt
 	#for f in "${GITLIST[@]}"; do
 	#	DRIVE="$( dirname $f )"
+	sed -i -e '/\/gits\/d' ~/x.txt
 while read line; do
 	DRIVE=$( dirname $line );
 	cd $DRIVE;
