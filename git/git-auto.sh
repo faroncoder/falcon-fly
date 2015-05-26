014039e16e328516bbbb6a9bfcfb793d26ecb1e5
faroncoder <faronledger@gmail.com>
2015-05-26 13:45:09 -0400

    auto-committing via script

--- git/git-auto.sh
+++ git/git-auto.sh
@@ -32,5 +32,4 @@ done < ~/x.txt
 #ret=$?; printf "`times`" >> /home/faron/.falcon/logs/scripts.log; exit "$ret"; exit 0
 
 stopred=`date +%s`; faronruntime=$(( $stopred - $startgreen )); echo "$0 | $startgreen | $stopred | $faronruntime " >> ~/.falcon/logs/scripts.log; exit 0
-times >> /home/faron/.falcon/logs/scripts.log
 
