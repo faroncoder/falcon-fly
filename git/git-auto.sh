c7a65583706dc0eb70d1968a694b6394cfd81a9f
faroncoder <faronledger@gmail.com>
2015-05-26 13:16:47 -0400

    auto-committing via script

--- git/git-auto.sh
+++ git/git-auto.sh
@@ -25,4 +25,4 @@ done < ~/x.txt
 
 
 #stopred=`date +%s`; faronruntime=$(( $stopred - $startgreen )); echo "$0 | $startgreen | $stopred | $faronruntime " >> ~/.falcon/logs/scripts.log; exit 0
-echo "$0 | `trap time SIGINT;`" >> ~/.falcon/logs/scripts.log; exit 0
+echo "$0 | $( trap 'time' SIGINT )" >> ~/.falcon/logs/scripts.log; exit 0
