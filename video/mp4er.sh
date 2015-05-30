e671b0044c259411b0ee8c63b0f067b9d4374576
faroncoder <faronledger@gmail.com>
2015-05-30 14:19:57 -0400

    auto-committing via script

--- video/mp4er.sh
+++ video/mp4er.sh
@@ -6,94 +6,106 @@ function stopwatchtime() {
 	echo "$0 | $startgreen | $stopred | $faronruntime " >> ~/.falcon/logs/scripts.log
 	exit 0
 }
-#### IGNORE ABOVE -
+#### IGNORE ABOVE
 export PATH
 PATHDIR="/home/faron/var/Streamings/files/engine/factory-mp4"
 cd $PATHDIR
-INPUT=$( find "$PATHDIR" -maxdepth 1 -type f -name '*.mkv' -exec basename {} \; | sort | head -n 1 )
+
+## only have one option for now which is libx264. future development will add few more libraries to the choice such as libraries for webm, flv, etc.
+CODECVID="libx264"
+## 1 = ultrafast | 2 = fast | 3 = medium | 4 = veryslow
+PRESET="1"
+## YES = 1 | NO = NULL
+FORCINGHIGHPROFILE=""
+CRF="25"
+## Video resolution.  360 for 360p; 480 for 480p; 720 for 720p, etc
+HEIGHTWT="360"
+## This provokes the VBV method in ffmpeg
+## BEGING of VBV
+MAXRAT="896k"
+BUFRAT="800k"
+BITRAT="800k"
+## END OF VBV
+## Let's define variables for ffmpeg
+TITLEFILE=" -faronized-"
+YEARFILE=""
+ALBUMFILE=""
+CONTAINTERFILE=""
+ARTISTFILE=""
 
 function ffmpegengine() {
-		HEIGHTWT="360"
-		TITLEFILE=""
-		YEARFILE=""
-		ALBUMFILE=""
-		CONTAINTERFILE=""
-		ARTISTFILE=""
-		MAXRAT="896k"
-		BUFRAT="800k"
-		BITRAT="800k"
-		CRF="25"
-		PRESET="ultrafast"
-		COMMENTFILE="Falcon $( date ) - $0"
-		if [[ "$( hostname )" == "f10" ]]; then
-                        CODECAID="libfdk_aac"
-                else
-                        CODECAID="libvo_aacenc"
-                fi
-
-		ffmpeg -i "$INPUT" -y \
-			-vcodec libx264 \
-			-preset "$PRESET" \
-			-maxrate "$MAXRAT" \
-			-bufsize "$BUFRAT" \
-			-b:v "$BITRAT" \
-			-vf "scale=trunc(oh*a/2)*2:$HEIGHTWT" \
-			-crf "$CRF" \
-			-tune zerolatency \
-			-g 250 \
-			-coder 1 \
-			-cmp chroma \
-			-partitions +parti4x4+partp8x8+partb8x8 \
-			-me_method hex \
-			-subq 6 \
-			-me_range 16 \
-			-keyint_min 25 \
-			-qp 0 \
-			-sc_threshold 40 \
-			-i_qfactor 0.71 \
-			-b_strategy 1 \
-			-flags +loop \
-			-flags +global_header \
-			-movflags +faststart \
-			-pix_fmt +yuv420p \
-			-acodec "$CODECAID" \
-			-ab 128k \
-			-ar 44100 \
-			-ac 2 \
-			-metadata title="$TITLEFILE" \
-			-metadata album="$ALBUMFILE" \
-			-metadata year="$YEARFILE" \
-			-metadata container="$CONTAINTERFILE" \
-			-metadata artist="$ARTISTFILE" \
-			-metadata comment="$COMMENTFILE" \
-			-f mp4 "$PREFILE.mp4" < /dev/null
-			mv "$PREFILE.mkv" completed/
-			mv "$PREFILE.mp4" output/
+	## Let's meet pre-requestions before firing ffmpeg.  Let's identify which server we are residenting in:
+	if [[ "$( hostname )" == "f10" ]]; then
+                    ## if this host is server A, let use this library
+                    CODECAID="libfdk_aac"
+            else
+            		## If this host is not Server A, we'll use different library.
+                    CODECAID="libvo_aacenc"
+    fi
+    if [[ "$CODECVID" == "libx264" ]]; then
+    		COND1="-tune zerolatency -qp 0"
+    		## Defining our codes for libx264
+        	if [[ "$PRESET" == "1" ]]; then
+					PRESETx264="ultrafast $COND1"
+        	fi
+	        if [[ "$PRESET" == "2" ]]; then
+					PRESETx264="fast"
+	        fi
+	        if [[ "$PRESET" == "3" ]]; then
+					PRESETx264="medium"
+	        fi
+	        if [[ "$PRESET" == "4" ]]; then
+					PRESETx264="veryslow $COND1"
+	        fi
+	        ## Triggering a flag whether to use -profile high or not
+	        if [[ -z "$FORCINGHIGHPROFILE" ]] || [[ ! "$FORCINGHIGHPROFILE" ]]; then
+	        			FORCEPROFILE=""
+	        	else
+	        			FORCEPROFILE="-profile high"
+	        fi
+	        CODECVCOMMANDS=" -vcodec $CODECVID -preset $PRESETx264 -crf $CRF $FORCEPROFILE"
+	        FILETYPE="mp4"
+	fi
+	COMMENTFILE="Falcon $( date ) - $0"
+	ffmpeg -i $INPUT -y $CODECVCOMMANDS \
+		-maxrate $MAXRAT -bufsize $BUFRAT -b:v $BITRAT \
+		-vf "scale=trunc(oh*a/2)*2:$HEIGHTWT" \
+		-flags +loop -flags +global_header -movflags +faststart  \
+		-pix_fmt +yuv420p -g 250 -coder 1 -cmp chroma \
+		-partitions +parti4x4+partp8x8+partb8x8 \
+		-me_method hex -subq 6 -me_range 16 -keyint_min 25 -sc_threshold 40 \
+		-i_qfactor 0.71 -b_strategy 1 \
+		-acodec "$CODECAID" -b:a 128k -ar 44100 -ac 2 \
+		-metadata title="$TITLEFILE" \
+		-metadata album="$ALBUMFILE" \
+		-metadata year="$YEARFILE" \
+		-metadata container="$CONTAINTERFILE" \
+		-metadata artist="$ARTISTFILE" \
+		-metadata comment="$COMMENTFILE" \
+		-f $FILETYPE $PREFILE.$FILETYPE < /dev/null
+		#Lets clean up - sending input file to 'completed' folder
+		mv "$PREFILE.mkv" completed/
+		## Sending new processed file to 'output' folder
+		mv "$PREFILE.$FILETYPE" output/
 }
 
-## if we do have a file to process
+## Let's see if we have file to process by command 'find'
+INPUT=$( find "$PATHDIR" -maxdepth 1 -type f -name '*.mkv' -exec basename {} \; | sort | head -n 1 )
+
+## Supposedly INPUT returns as true (found a file)
 if [[ ! -z "$INPUT" ]]; then
 		## we grab filename of the file without extension
         PREFILE="$( rev <<< $INPUT | cut -d "." -f2 | rev )"
-        ## start ffmpeg
+        ## start ffmpeg function
         ffmpegengine
+    ## if INPUT returns as false ( found a file )
     else
-    	## Let's check whether we have file or not; via sort/find by filename
+    	## Let's double check if we actually do not have file to process to be safe than sorry (via sort/find/extracting filename from file )
     	if [[ -z "$PREFILE" ]]; then
-		## if there is no filename, let's exit via logger.
+		## if there is no filename, then we are satisfied and exit via logger.
 		stopwatchtime
     	fi
 fi
-
-# 		#          	if [ -z "$( ls *.mkv )" ]; then
-#                 INPUT="$PATHDIR/$PREFILE.mkv"
-#                 cp "/media/mkv/$PREFILE.mkv" $PATHDIR/
-
-
-# fi
-# #		INPUT="$PATHDIR/$PREFILE.mkv"
-#		cp "/media/mkv/$FILEGRAB" $PATHDIR/
-
 
 #ffmpeg -ss 00:00:10.435 -i "output/$PREFILE.mp4" -y -f  image2 -vframes 1 "$PREFILE.png"  < /dev/null
 #convert "$PREFILE.png" -resize "330x248^" -gravity center -background black -extent 330x248 "thumbs/$INPUT.png" < /dev/null
@@ -102,7 +114,7 @@ fi
 #ffmpegthumbnailer -i "output/$PREFILE.mp4" -w -c png -s 0 -o "thumbs/$INPUT.png"
 
 
-## script self-initation to loop.
-./$0
+## script self-executes so it'd loop to finish all of files.
+$0
 stopwatchtime
-### Faron's note:  I didn't use loop via array for this one. We all know that FFMPEG does take time to finish processing one file at a rate of movie's length time which it'll stands as an ETA for FMPEG to finish processing.  I.e. if mkv has a movie stream and it's movie time is 1hr and 30min; therefore it would take 1hr and 30min (more or less) for FFMPEG to finish processing it [ x264 is huge reason ].  So, if I did use array method to loop / process my files, at anytime I break the code via CTRL-C, script will stop processing the file, but it'll still move on to next movie to process.  It'll have to be stopped via CTRL-Z.  I prefer using CTRL-C since it is how I am able to track all of my scripts meanwhile CTRL-Z only kill the script but leaving with no footprints - thus harder for me to track (and it opens the door for bugs).  This method simply process the file at a time, and script will exit  everytime it finishes processing a file, however script will also self-execute so, it'd loop to next file leaving footprints which are trackable.
+### Faron's note:  I didn't use loop via array for this one. FFMPEG does take time to finish processing one file at a rate of movie's length time which it'll stands as an ETA for FMPEG to finish processing.  I.e. if mkv has a movie stream and its movie time is 1hr and 30min; therefore ETA for FFMPEG to finish would be 1hr and 30min (more or less) [ x264 is huge reason ].  So, if I did use array method to loop / process files, when I break the code via CTRL-C, script will stop processing the file, but it'll still move on to next movie to process.  It'll have to be stopped via CTRL-Z.  I prefer using CTRL-C in order to be able to track for various reasons however CTRL-Z only kill the script but leaving with no footprints - thus harder for me to track (and it opens the door for bugs).  This method simply process the file at a time, and will exit but, also self-execute so, it'd loop to next file leaving footprints which are trackable.
