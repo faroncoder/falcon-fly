2a51ee872ea86bbece8279f6c510e92ece7d0e67
faroncoder <faronledger@gmail.com>
2015-05-31 12:15:03 -0400

    auto-committing via script

--- video/ffmpeg.intransfer.file.sh
+++ video/ffmpeg.intransfer.file.sh
@@ -17,29 +17,33 @@ if [ ! -d "$PATHLINK/streamer" ]; then
 fi
 
 
-scp "faron@192.168.1.6:$PATHLINK/{*.mp4,*.dat}" "$PATHLINK/"
+scp faron@192.168.1.6:$PATHLINK/{*.mp4,*.dat} ./
 #scp "faron@192.168.1.2:$PATHLINK/{*.mp4,*.dat}" "$PATHLINK/"
 GETDAT=( $( find -maxdepth 1 -type f -name '*.dat' -exec basename {} \; | sort ) )
+> delete.list
 for f in "${GETDAT[@]}"; do
 	PREFILE="$( rev <<< $f | cut -d "." -f2 | rev )"
-	INFILE=$( md5sum $PREFILE.mp4 ) | awk '{ print $1 }'
+	INFILE=$( md5sum $PREFILE.mp4 | awk '{ print $1 }' )
 	CHECKFILE=$( cat $PREFILE.dat | head -n 1 | awk '{ print $1 }' )
 	if [[ "$INFILE"="$CHECKFILE" ]]; then
 		##if we have a match - move the file into archive
 		mv "$PREFILE.mp4" streamer/
 		echo "$PREFILE.mp4 is clean" >> $PWD/result.txt
 		#create blank file with same name - aka decoy-kill
-		touch $PREFILE.mp4
+		echo "$PREFILE.mp4" >> delete.list
+		echo "$PREFILE.dat" >> delete.list
 		#sending decoy-kill file to server
-		scp "$PREFILE.mp4" "faron@{192.168.1.6,192.168.1.2}:$PATHLINK/"
+		scp "$PREFILE.mp4" "faron@192.168.1.6:$PATHLINK/"
 		#remove decoy-kill and dat file to complete the process
-		rm $PREFILE.mp4 $f
+		rm "$PREFILE.mp4" $f
 	else
 		##if we dont have match - delete the file
 		echo "$PREFILE.mp4 is corrputed" >> $PWD/result.txt
 		rm $PREFILE.mp4 $f
 	fi
 done
+scp delete.list "faron@192.168.1.6:$PATHLINK/"
+rm delete.list
 ## END
 stopwatchtime
 
