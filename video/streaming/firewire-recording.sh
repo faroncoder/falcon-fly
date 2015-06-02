d85d52e0ffad1038b524d7a82f11a20d386efe01
faroncoder <faronledger@gmail.com>
2015-06-02 12:15:02 -0400

    auto-committing via script

--- video/streaming/firewire-recording.sh
+++ video/streaming/firewire-recording.sh
@@ -12,6 +12,6 @@ if [[ ! -z "nohup.out" ]]; then
 fi
 
 
-nohup `$( dvgrab -noavc -format dv2 -guid 1 - | ffmpeg -re -i - -y -c:v libx264 -preset ultrafast -pix_fmt yuv420p -crf 25.0 -qp 0 -g 60 -c:a libfdk_aac -f mpeg "$HOME/$file.mpg" >/dev/null )` & sleep 3; ffplay $HOME/$file.mpg
+nohup `$( dvgrab -noavc -format dv2 -guid 1 - | ffmpeg -re -i - -y -c:v libx264 -preset ultrafast -pix_fmt yuv420p -crf 25.0 -tune zerolatency -qp 0 -g 60 -b:v 800k -maxrate 1024k -bufsize 800k -flags +global_header -c:a libfdk_aac -b:a 128k -ar 44100 -ac 2 -f mpeg "$HOME/$file.mpeg" >/dev/null )` & sleep 2 & ffplay $HOME/$file.mpeg
 
 stopred=`date +%s`; faronruntime=$(( $stopred - $startgreen )); echo "$0 | $startgreen | $stopred | $faronruntime " >> ~/.falcon/logs/scripts.log; exit 0
