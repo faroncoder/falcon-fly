a537501f017ff72a4eb220c3f4d9588e289a574c
faroncoder <faronledger@gmail.com>
2015-06-03 18:15:04 -0400

    auto-committing via script

--- video/streaming/firewire-recording.sh
+++ video/streaming/firewire-recording.sh
@@ -10,4 +10,13 @@ file=`uuid | sed 's/-//g'`
 #sleep 2; ffplay "$MYHOME/$file.mpeg"
 
 
-dvgrab -noavc -format dv2 -guid 1 - | ffmpeg -re -i - -c:v libx264 -preset ultrafast -maxrate 1024k -bufsize 1229k -qp 0 -pix_fmt yuv420p -tune zerolatency -crf 24 -g 60 -flags +loop -flags +global_header -movflags +faststart -c:a libfdk_aac -b:a 128k -ar 44100 -ac 2 -f asf - | ffplay - < /dev/null
+#dvgrab -noavc -format avi -guid 1 - | ffmpeg -v error -re -f avi -i - -y -c:v libx264 -preset ultrafast -profile baseline -qp 0 -pix_fmt yuv420p -tune zerolatency -crf 25 -g 60 -maxrate 1024k -bufsize 1229k -c:a libmp3lame -b:a 128k -ar 44100 -ac 2 -f mpg "$MYHOME/$file.mpeg" < /dev/null
+
+dvgrab -noavc -format dv2 -guid 1 - | ffmpeg -re -i - -vcodec libx264 -preset ultrafast -maxrate 1024k -bufsize 1024k -crf 25.0 -qp 0 -tune zerolatency -g 60 -pix_fmt yuv420p -flags +loop -flags +global_header -acodec libfdk_aac -b:a 128k -ar 44100 -ac 2 -f asf $file.avi > /dev/null & sleep 2; ffplay $file.avi;
+
+
+#dvgrab -noavc -format dv2 -guid 1 - | ffmpeg -re -i - -c:v libx264 -preset ultrafast -qp 0 -pix_fmt yuv420p -tune zerolatency -maxrate 1024k -bufsize 1024k -crf 24 -c:a libfdk_aac -ac 2 -ar 44100 -b:a 128k -f asf "$file.avi" > /dev/null & sleep 2; ffplay "$MYHOME/$file.avi";
+
+exit 0
+
+
