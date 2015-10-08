747284f5f8554791c7a9d6a9b29a54bd4a2ba19d
faroncoder <faronledger@gmail.com>
2015-10-08 05:10:23 -0400

    updating

--- newbin/ff.flu.2.sh
+++ newbin/ff.flu.2.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 startgreen=`date +%s`
 #sudo flumotion-worker -d 3 -v -T tcp  -u faron -p great /etc/flumotion/workers/default.xml
-flumotion-worker -d 3 -v -T tcp  -u faron -p great /etc/flumotion/workers/default.xml
+#flumotion-worker -d 3 -v -T tcp  -u faron -p great /etc/flumotion/workers/default.xml
+flumotion-worker -d 3 -v -T tcp  -u faron -p great -H -T tcp 192.168.1.6 -P 8642
 
 stopred=`date +%s`; faronruntime=$(( $stopred - $startgreen )); echo "$0 | $startgreen | $stopred | $faronruntime " >> ~/.falcon/logs/scripts.log; exit 0
 
