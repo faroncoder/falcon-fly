c9b19476a46e3a3a8fcf1c077942ca4e4f15c371
faroncoder <faronledger@gmail.com>
2016-02-06 11:45:34 -0500

    auto-f10-Sat Feb  6 11:45:33 EST 2016

--- htmls/ff.hl.cssglop.sh
+++ htmls/ff.hl.cssglop.sh
@@ -1,13 +1,44 @@
 #!/bin/bash
-CSSGLOP=(  `find -L $PWD -maxdepth 1 -type f -name '*.css' ! -name 'cssengine.css'` )
+PROJ=$1
+if [ -z "$PROJ" ]; then
+	PROJ=$PWD
+fi
+# else
+# 	PROJ=$( find -L $PROJ -type d -name "*jsc" -exec dirname {} \; )
+# fi
+if [ -d "$PROJ/jsc" ]; then
+		JSCLOC="$PROJ/jsc"
+		if [ -d "$PROJ/.jsc" ];	then
+			JSCLOC="$PROJ/.jsc"
+		fi
+	else
+		echo "No JSC. Exiting."
+		exit 1
+fi
+# PROJsc=$( find -L $PROJ -type d -name "*jsc" -exec basename {} \; )
 TYPE="css"
-FILEPOINT="$PWD/cssengine.$TYPE"
+FILEPOINT="cssengine.$TYPE"
+DOME="$JSCLOC/$TYPE"
+if [ -d "$DOME" ];
+		then
+		echo "Entering css folder."
+		cd $DOME
+else
+		if [ ! -d "$DOME" ]; then
+			echo "No JSC.  Exiting."
+			exit 2
+		fi
+fi
+
+CSSGLOP=$( find -L . -maxdepth 1 -type f -name '*.css' ! -name 'cssengine.css' )
+
 glopperGo() {
 		RESULT=$( echo ${CSSGLOP[@]} | wc -w )
 		echo "$RESULT $TYPE files glopped"
-	}
-CSSGLOPPER=( `find -L $PWD -maxdepth 1 -type f -name '*.css' ! -name 'cssengine*' -exec basename {} \; ` )
-if [[  -z "$CSSGLOPPER" ]]; then
+}
+
+CSSGLOPPER=( $( find -L . -maxdepth 1 -type f -name '*.css' ! -name 'cssengine*' -exec basename {} \; ) )
+if [[ -z "$CSSGLOPPER" ]]; then
 	echo "No $TYPE files to glop"
 else
 	> $FILEPOINT
@@ -17,5 +48,9 @@ else
 		INJECTION="@import url(`printf  \"'$b'\"`);"
 	 	echo $INJECTION >> "$FILEPOINT"
 	done
+	glopperGo
 fi
+cd $PROJ
+
 exit 0
+
--- htmls/ff.hl.jsglop.sh
+++ htmls/ff.hl.jsglop.sh
@@ -1,37 +1,69 @@
 #!/bin/bash
-JSGLOP=(  `find -L $PWD -maxdepth 1 -type f -name '*.js' ` )
+PROJ=$1
+if [ -z "$PROJ" ]; then
+	PROJ=$PWD
+fi
+# else
+# 	PROJ=$( find -L $PROJ -type d -name "*jsc" -exec dirname {} \; )
+# fi
+if [ -d "$PROJ/jsc" ]; then
+		JSCLOC="jsc"
+		if [ -d "$PROJ/.jsc" ];	then
+			JSCLOC=".jsc"
+		fi
+	else
+		echo "No JSC. Exiting."
+		exit 1
+fi
+# PROJsc=$( find -L $PROJ -type d -name "*jsc" -exec basename {} \; )
 TYPE="js"
-FILEPOINT="$PWD/appengine.$TYPE"
-glopperGo() {
-		RESULT=$( grep 'loaded' "$FILEPOINT" | wc -l )
-		echo "FALCON: Gloped $RESULT $TYPE files."
-	}
-
-if [[  ! "$JSGLOP" ]]; then
-	echo "No JS files to glop"
+FILEPOINT="appengine.$TYPE"
+DOME="$PROJ/$JSCLOC/$TYPE"
+if [ -d "$DOME" ]; then
+		echo "Entering js folder."
+		cd $DOME
 else
+		if [ ! -d "$DOME" ]; then
+			echo "No JSC.  Exiting."
+			exit 2
+		fi
+fi
 
+JSGLOP=$( find -L . -maxdepth 1 -type f -name '*.js' ! -name 'appengine.js' )
+
+glopperGo() {
+		RESULT=$( echo ${JSGLOP[@]} | wc -w )
+		echo "$RESULT $TYPE files glopped"
+}
+
+JSGLOPPER=( ` find -L . -maxdepth 1 -type f -name '*.js' ! -name 'appengine*' -exec basename {} \; ` )
+if [[ -z "$JSGLOPPER" ]]; then
+	echo "No $TYPE files to glop"
+else
 	> $FILEPOINT
 	echo "
 	\$.getScript(\"http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js\", function(data, textStatus) {
 		console.log(\"bootstrap.min.js loaded\", textStatus);
 	});" >> $FILEPOINT
-	#fi
-	CHECKANGULAR=$( find -L $PWD -maxdepth 1 -type f -name 'angular*.js' -exec basename \; )
-	if [ -f "$CHECKANGULAR" ]; then
-	echo "
-	\$.getScript(\"//.priv/jsc/js/$CHECKANGULAR\", function(data, textStatus) {
-		console.log(\"$CHECKANGULAR\", textStatus);
-	});" >> $FILEPOINT
-	fi
-	JSGLOPPER=( `find -L $PATHDIRJSC -maxdepth 1 -type f -name '*.js' ! -name 'appengine.js' ! -name 'bootstrap.min.js' ! -name 'angular.min.js' ! -name '*ie*.js'  -exec basename {} \; ` )
-
+	# CHECKANGULAR=$( find -L . -maxdepth 1 -type f -name 'angular*.js' -exec basename \; )
+	# if [ -f "$CHECKANGULAR" ]; then
+	# echo "
+	# \$.getScript(\"//jsc/js/$CHECKANGULAR\", function(data, textStatus) {
+	# 	console.log(\"$CHECKANGULAR\" loaded, textStatus);
+	# });" >> $FILEPOINT
+	# fi
+	JSGLOPPER=( $( find -L . -maxdepth 1 -type f -name '*.js' ! -name 'appengine*' ! -name 'bootstrap*.js' ! -name 'angular*.js' ! -name '*ie*.js' ) )
 		for w in "${JSGLOPPER[@]}"; do
+	 		FILEGET=$( basename $w )
 	 		INPUTLOAD=$( echo "
-	\$.getScript(\"//.priv/jsc/js/$w\", function(data, textStatus) {
-		console.log(\"$w loaded\", textStatus);
+	\$.getScript(\"//jsc/js/$FILEGET\", function(data, textStatus) {
+		console.log(\"$FILEGET loaded\", textStatus);
 	});" )
 			echo -e "$INPUTLOAD" >> $FILEPOINT
 		done
+	glopperGo
 fi
+cd $PROJ
+
 exit 0
+
