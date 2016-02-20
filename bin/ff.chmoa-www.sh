0a80c113d24a8a2dc59189001e1cda38ffece6ef
faroncoder <faronledger@gmail.com>
2016-02-20 10:40:09 -0500

    auto-f10-Sat Feb 20 10:40:08 EST 2016

--- bin/ff.chmoa-www.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-WWWD=/srv/www/htdocs
-
-sudo chown -R faron:www-data $WWWD
-sudo chmod -R 665 $WWWD
-
-exit 0
--- newbin/www-chown-modizer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-/bin/chown -R www-data:www-data /srv/www/htdocs
-/bin/chmod -R 775 /srv/www/htdocs
-
-exit 0
-
