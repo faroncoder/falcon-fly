35328b070f73c34fdceca93fa6484fc3ee0ff7c6
faroncoder <faronledger@gmail.com>
2016-02-22 13:56:10 -0500

    f10-merge

--- git/ff.git.update.sh
+++ git/ff.git.update.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
-export PATH=$PATH
-HERE=$PWD
-startgreen=`date +%s`
-stopwatchtime(){
-  stopred=`date +%s`
-  faronruntime=$( echo "$(( $stopred - $startgreen ))" )
-  echo "$0 | $startgreen | $stopred | $faronruntime " >> /home/faron/.falcon/logs/scripts.log
-  exit 0
-}
 
+#!/bin/bash
+RETURN=$PWD
+if [ ! "$( echo $PATH | grep '/usr/local/bin' )" ]; then export PATH=$PATH:/usr/local/bin; fi
+fcbk="$(tput setaf 0)"; fcr="$(tput setaf 1)"; fcg="$(tput setaf 2)"; fcy="$(tput setaf 3)"; fcb="$(tput setaf 4)"; fcp="$(tput setaf 5)"; fcc="$(tput setaf 6)"; fcw="$(tput setaf 7)"; fco="$(tput sgr0)"; fcok="[$fcg OK $fco]"; fcer="[$fcr ERR $fco]";
+XeB=`date +%s`
+function XeF {
+XeE=`date +%s`; XeT=$( echo "$(( $XeB - $XeE ))" ); logger "$0 | $XeB | $XeE | $XeT "; exit 0
+}
+#if [[ "$1" != "" ]]; then
+#################### BEGIN
 opensh_engine(){
         echo "Initializing SSH agent..."
         chmod 700 -R ~/.ssh;
@@ -20,7 +20,6 @@ opensh_engine(){
         eval $( ssh-agent -s ) > ~/.ssh/environment;
         find -L "$HOME/.ssh" -type f -name 'id_*' ! -name '*.pub' -exec ssh-add {} \;
 }
-
 opensh_started(){
         PIDOFCHECK=$( pidof ssh-agent | wc -w )
         if [[ "$PIDOFCHECK" -gt 2 ]]; then
@@ -34,25 +33,11 @@ opensh_started(){
 }
 
 opensh_started
-
 git config --global push.default simple
-
-
-
-#GITLIST=( "$(find /home/faron/.falcon/scripting -maxdepth 3 -type d -name '.git' -exec dirname {} \; | sed '/\/gits\//d' )" )
-#GETALLGIT=( $( find /home/faron/.falcon/scripts -type d -name '.git' -exec dirname {} \;  ) )
 GETALLGIT=( $( find /home/faron/.falcon/scripts -type d -name '.git' -exec dirname {} \;  ) )
 
-for git in "${GETALLGIT[@]}";
-	do 
+for git in "${GETALLGIT[@]}"; do 
 	cd $git 2> /dev/null
-
-#CHECKNAME=$( grep 'faroncoder' $PWD/.git/config )
-#CHECKEMAIL=$( grep 'faronledger@gmail.com' $PWD/.git/config )
-
-#for p in "${GETALLGIT[@]}";
-#do
-#	cd $p 2> /dev/null
 	CKTHISGIT="$( basename $PWD )"
 	if [[ "$CKTHISGIT" = 'scripts' ]]; then
 		THISGIT="falcon-fly.git"
@@ -63,26 +48,8 @@ for git in "${GETALLGIT[@]}";
 	git config --global user.name "faroncoder"
 	git config --global user.email "faronledger@gmail.com"
 	sleep 1
-	
 	echo "FALCON: Updating $THISGIT"
 	sleep 1
-	# if [ ! "$CHECKNAME" ]; then
-	# 	git config --global user.name "faroncoder"
-	# fi
-	# if [ ! "$CHECKEMAIL" ]; then
-	# 	git config --global user.email faronledger@gmail.com
-	# fi
-
-	# if [ ! -d "/home/faron/.falcon/scripting/home/faron/.falcon-gits/`echo $p`/.git" ]; then
-	# 		git init
-	# fi
-	# CHECKCONF=$( grep 'url' /home/faron/.falcon/scripting/home/faron/.falcon-gits/$( echo $p )/.git/config )
-	# if [ -z "$CHECKCONF" ];
-		# then
-		#git config --global push.default simple
-
-	#fi
-	#git remote set-url origin "git@github.com:faroncoder/$THISGIT"
 	git fetch
 	git pull
 	git commit -a -m "$( hostname )-merge" 
@@ -90,51 +57,10 @@ for git in "${GETALLGIT[@]}";
 	git add -A
 	git commit -a -m "$( hostname )-update"
 	git push
-#done
 	echo -e $fcok
 done
-# echo "FALCON: Updating/home/faron/.falcon-fly"
-# cd /home/faron/.falcon/scripts
-# CHECKNAME=$( grep 'faroncoder' $PWD/.git/config )
-# 	CHECKEMAIL=$( grep 'faronledger@gmail.com' $PWD/.git/config )
-# 	if [ -z "$CHECKNAME" ];
-# 		then
-# 		git config --global user.name "faroncoder"
-# 	fi
-# 	if [ -z "$CHECKEMAIL" ];
-# 		then
-# 		git config --global user.email faronledger@gmail.com
-# 	fi
-# 	#git config --global push.default simple
-# 	#git remote add origin
-# 	git fetch
-# 	#git remote set-url origin git@github.com:faroncoder/home/faron/.falcon-fly.git
-# 	git pull
-# 	git status
-# 	git add --all
-# 	git commit -m "auto-committer"
-# 	git push
-
-# # find "/home/faron/.falcon/scripting" -maxdepth 3 ! -path '*/gits/*' -type d -name '.git' > /tmp/x.txt
-# 	#for f in "${GITLIST[@]}"; do
-# 	#	DRIVE="$( dirname $f )"
-# 	sed -i -e '/\/home\/faron\//home/faron/.falcon\/scripting\/gits/d' /tmp/x.txt
-# while read line;
-# 	do
-# 		DRIVE=$( dirname $line );
-# 		cd $DRIVE;
-# 		echo  "Updating git in $PWD";
-# 		git status;
-# 		git add --all;
-# 		git commit -m "auto-committing via script";
-# 		#git push -u "$( basename $PWD ).git" master;
-# 		#git push -u "$( basename $PWD ).git";
-# 		git push;
-# 	done < /tmp/x.txt
-# rm /tmp/x.txt
-################### END
-#cd $RETURN
-else echo -e "$fcer NADA =$fcy no gits $fco"; fi
+cd $RETURN
+## else echo -e "$fcer NADA =$fcy no gits $fco"; fi
 ### exit code for clean exit
 XeF
 ### IGNORE BELOW. THIS IS MEGATAG FOR MY SCRIPTS
