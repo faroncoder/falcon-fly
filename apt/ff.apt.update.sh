041234b9eb0f5ab44a99391504e4d72cb7d0e064
faroncoder <faronledger@gmail.com>
2016-02-27 23:20:20 -0500

    auto-f7-Sat Feb 27 23:20:20 EST 2016

--- apt/ff.apt.update.sh
+++ apt/ff.apt.update.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-startgreen=`date +%s`
+HERE=$PWD
+if [ ! "$( echo $PATH | grep '/usr/local/bin' )" ]; then
+	export PATH=$PATH:/usr/local/bin
+fi
+XeB=`date +%s`
+function XeF {
+XeE=`date +%s`; XeT=$( echo "$(( $XeB - $XeE ))" ); logger "$0 | $XeB | $XeE | $XeT "; exit 0
+}
+#if [ "$1" != "" ]; then
+#################### BEGIN
 
 sudo apt-get dist-upgrade
 sudo apt-get autoremove -y --force-yes
@@ -7,5 +16,14 @@ sudo apt-get autoclean -y --force-yes
 sudo apt-get clean -y --force-yes
 sudo apt-get update -y --force-yes
 
-stopred=`date +%s`; faronruntime=$(( $stopred - $startgreen )); echo "$0 | $startgreen | $stopred | $faronruntime " >> /home/faron/.falcon/logs/scripts.log; exit 0
+################### END
+cd $HERE
+#elif [ "$1" = '' ];
+#	then
+#  echo "usage: ff.apt.update "
+#  echo "example:  ff.apt.update  "
+#fi
+## TALON:
+XeF
+
 
