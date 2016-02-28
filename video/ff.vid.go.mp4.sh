8c5a8d1640862e7ba80f03fa9242ea7370c4c609
faroncoder <faronledger@gmail.com>
2016-02-28 00:54:40 -0500

    f10-update

--- video/ff.vid.go.mp4.sh
+++ video/ff.vid.go.mp4.sh
@@ -61,16 +61,6 @@ startFFMPEG() {
 		SIZEKILE=$( ls $x -s | awk '{print $5}' ) ## GET BYTE SIZE
  		INPUT=$( basename $x)
  		STEPEXT=$( rev <<< $INPUT | cut -d"." -f1 | rev )  ## GET FILE EXTENSION
-<<<<<<< HEAD
- 		STEPFULL=$( rev <<< $x | cut -d"." -f2 | rev | cut -d"_" -f1 )  ## GET FILE EXTENSION
-		STEPNUM=$( rev <<< "$x" | cut -d"."  -f2  | rev | cut -d"_" -f2 )  ## GET FILE EXTENSION
- 		#STEPNUM=( $( find . -maxdepth 1 -type f -name "*_$STEPFULL*" | wc -l ) ) ## CHECKS FOR SERIAL NUMBER 
-					if [[ "$STEPNUM" = 0 ]]; then   ## IF THERE IS NO SERIAL NUMBER
-						SEQ=$( seq -w 0 0 | tail -n 1 )  ##ASSIGN SERIAL NUMBER
-					else
-						SEQ=$( seq -w 0 `echo "$(( $STEPNUM + 1 ))"` | tail -n 1 )  #IF THERE IS SERIAL NUMBER THEN ROTATE THE NUMBER ONE UP
-					fi
-=======
  		STEPFULL=$( rev <<< $INPUT | cut -d"." -f2 | rev | cut -d"_" -f1 )  ## GET FILE EXTENSION
 		STEPNUM=$( rev <<< "$INPUT" | cut -d"."  -f2  | rev | cut -d"_" -f2 )  ## GET FILE EXTENSION
  		#STEPNUM=( $( find . -maxdepth 1 -type f -name "*_$STEPFULL*" | wc -l ) ) ## CHECKS FOR SERIAL NUMBER 
@@ -79,7 +69,7 @@ startFFMPEG() {
 					#else
 					#	SEQ=$( seq -w 0 `echo "$(( $STEPNUM + 1 ))"` | tail -n 1 )  #IF THERE IS SERIAL NUMBER THEN ROTATE THE NUMBER ONE UP
 					#fi
->>>>>>> 43f4c279b1b6894ccbffa06ed3ac99d7a61682e7
+
 		TITLEFILE=""
 		YEARFILE=""
 		ALBUMFILE=""
@@ -87,18 +77,6 @@ startFFMPEG() {
 		ARTISTFILE=""
 		COMMENTFILE=""
 		OUTPUTA="$STEPFULL"
-<<<<<<< HEAD
-		OUTPUT="$STEPFULL-$SEQ.mp4"
-		EXIT="$STEPFULL-$SEQ-done-$SEAF.$STEPEXT"
-		EXITA="$STEPFULL-$STEPNUM.$STEPEXT"
-	        			#DEGREE=$( echo $(( $SIZEKILE - $KNFILE )) )
-
-
-		        			ffmpeg -r 25 -i $x -y -codec:v $CODECVID -preset $CALCULATED -maxrate "$( echo $MAXRAT)k" -bufsize "$( echo $BUFRAT)k" -b:v "$( echo $BITRAT)k" -crf $CRF -pix_fmt +yuv420p -vf "scale=trunc(oh*a/2)*2:$HEIGHTWT" -coder 1 -flags +loop -flags +global_header -movflags +faststart -x264opts keyint=600:min-keyint=30:bframes=16 -cmp chroma -partitions +parti4x4+partp8x8+partb8x8 -me_method hex -subq 6 -me_range 16 -g 59.94 -keyint_min 25 -sc_threshold 40 -i_qfactor 0.71 -b_strategy 1 -threads 0 -codec:a aac -strict -2 -ar 44100 -ac 2 -ab 128k -f mp4 "$OUTPUTA" < /dev/null
-			        		#INFILE=$( ls $OUTPUT -lh | awk '{ print $5 }' )  #GET FILE SIZE RESULT
-			        		TITLEFILE="BIT=$BITRAT MAX=$MAXRAT BUF=$BUFRAT "
-							KNFILE="$( ls $OUTPUTA -lh | awk '{ print $5 }' )"  #GET FILE SIZE RESULT			        		
-=======
 		OUTPUT="$STEPFULL.mp4"
 		# EXIT="$STEPFULL.$STEPEXT"
 		#EXITA="$STEPFULL-done.$STEPEXT"
@@ -110,26 +88,10 @@ startFFMPEG() {
 			        		INFILE=$( ls $OUTPUT -lh | awk '{ print $5 }' )  #GET FILE SIZE RESULT
 			        		TITLEFILE="BIT=$BITRAT MAX=$MAXRAT BUF=$BUFRAT "
 							KNFILE="$( ls $OUTPUTA -lh | awk '{ print $5 }' )"  #GET FILE SIZE RESULT			        		
-						
->>>>>>> 43f4c279b1b6894ccbffa06ed3ac99d7a61682e7
 							ALBUMFILE=""
 							CONTAINTERFILE="MP4/QT/FFMPEG"
 							ARTISTFILE="$( echo $THISVERSION )"
 							COMMENTFILE="PST=$PRESETx264 CRF=$CRFSET OLD=$SIZEFILE NEW=$KNFILE"
-<<<<<<< HEAD
-			        		#echo $(( $SIZEKILE - $KNFILE ))
-							OUTFILE="converted: ID=$SEAF FILE=$x  IN=$SIZEFILE  OUT=$KNFILE  version=$THISVERSION" #LOG FORMAT
-							mv $x "done/$EXIT" 2> /dev/null		#
-							rename "s/-/_/g" "done/$EXIT"  #DISABLE FILE LOCK
-							ffmpeg -i $OUTPUTA -codec copy -metadata title="$TITLEFILE" -metadata album="$ALBUMFILE" -metadata year="$YEARFILE" -metadata container="$CONTAINTERFILE" -metadata artist="$ARTISTFILE" -metadata comment="$COMMENTFILE" -f mp4  "$OUTPUT" < /dev/null
-							rm $OUTPUTA
-							mv "$OUTPUT" "mp4/$OUTPUT"
-							rename "s/-/_/g" "mp4/$OUTPUT"  #DISABLE FILE LOCK
-
-	        				echo $OUTFILE >> .result_converted #SEND TO LOG
-	        done
-
-=======
 			        		
 			        		#echo $(( $SIZEKILE - $KNFILE ))
 							OUTFILE="converted: ID=$SEAF FILE=$x  IN=$SIZEFILE  OUT=$KNFILE  version=$THISVERSION" #LOG FORMAT
@@ -146,15 +108,11 @@ startFFMPEG() {
 	        				echo $OUTFILE >> .result_converted #SEND TO LOG
 	        done
 		
->>>>>>> 43f4c279b1b6894ccbffa06ed3ac99d7a61682e7
 }
         startFFMPEG
 
 
 
 exit 0
-<<<<<<< HEAD
 ## VER=730ebd32-d446-11e5-b1f0-7bf895f2366a
-=======
-## VER=730ebd32-d446-11e5-b1f0-7bf895f2366a
->>>>>>> 43f4c279b1b6894ccbffa06ed3ac99d7a61682e7
+
