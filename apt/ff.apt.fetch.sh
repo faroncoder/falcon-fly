5844b78d69cf6b6c1f7f4d5feea2f8a149b65879
faroncoder <faronledger@gmail.com>
2016-06-26 19:30:14 -0400

    Sun Jun 26 19:30:14 EDT 2016

--- apt/ff.apt.fetch.sh
+++ apt/ff.apt.fetch.sh
@@ -32,100 +32,67 @@ done
 APID="$( uuid )_f"
 
 defaultFunction(){
-<<<<<<< HEAD
 	sudo rm /var/lib/dpkg/lock; sudo dpkg --configure -a;
-=======
-	sudo rm /var/lib/dpkg/lock;
-	sudo dpkg --configure -a;
->>>>>>> 1e8affc5bfb91802d90196acf1c34ab90c678927
+
 }
 
 beginInstall(){
 	defaultFunction
-<<<<<<< HEAD
+
 	sudo apt-get install -y  $appget
-=======
-<<<<<<< HEAD
-	sudo apt-get install -y $appget
-=======
 	cat "/mnt/falcon/files/configs/etc_apt-get_packages.list" | uniq | sort > /tmp/aptgrab_pkg
 	echo "$appget" >> /tmp/aptgrab_pkg
 	less "/tmp/aptgrab_pkg" | uniq | sort > /mnt/falcon/files/configs/etc_apt-get_packages.list
 	logger "FARON:: apt-get package recorded for installation = $appget "
 	sudo apt-get install -y $appget --force-yes
->>>>>>> 1e8affc5bfb91802d90196acf1c34ab90c678927
->>>>>>> 6224fb5b175399210b72de5d5294ce9546f28ecf
 }
 
 beginBuild(){
 	defaultFunction
-<<<<<<< HEAD
-	sudo apt-get build-dep -y $appget
-=======
-<<<<<<< HEAD
 	sudo apt-get build-dep -y --force-yes $appget
->>>>>>> 6224fb5b175399210b72de5d5294ce9546f28ecf
 }
 beginCheck(){
 	defaultFunction
 	beginInstall < /dev/null >> /tmp/$APID
 }
-=======
-	sudo apt-get build-dep $appget -y --force-yes
-}
 #beginCheck(){
 #	defaultFunction
 #	beginInstall < /dev/null >> /tmp/$APID
 #}
->>>>>>> 1e8affc5bfb91802d90196acf1c34ab90c678927
 
 appCheck(){
 	if [ -f "/tmp/$APID" ]; then
 		while read line; do
 			echo $line
 		done < /tmp/$APID
-<<<<<<< HEAD
 	else
-		echo "no dependency needed"	
+		echo "no dependency needed"
 		> /tmp/$APID
-=======
 #	else
 #		echo "no dependency needed"
 #		> /tmp/$APID
->>>>>>> 1e8affc5bfb91802d90196acf1c34ab90c678927
 	fi
 }
 
 appGo(){
-<<<<<<< HEAD
-	PACKS=( $STRINGCOL )
-	for appget in "${PACKS[@]}"; do
-			beginBuild
-			beginCheck
-			appCheck
-=======
+
 	PACKS=( `echo ${STRINGCOL[@]}` )
 	for appget in "${PACKS[@]}"; do
 			beginBuild
 #			beginCheck
 #			appCheck
->>>>>>> 1e8affc5bfb91802d90196acf1c34ab90c678927
-			beginInstall
 	done
 	appget=""
 }
 appGo
-<<<<<<< HEAD
-=======
 
->>>>>>> 1e8affc5bfb91802d90196acf1c34ab90c678927
 # 		# GETPACK=( "$( echo \"$( sudo apt-get install $appget < /dev/null" |  sed  -n  -e '{ /Suggested packages/,/The following NEW/p }' | sed '/The following/d' )\" )" )
 # 		# GETPACK=$( sudo apt-get install $appget 2< /dev/null  |  sed  -n  -e '{ /Suggested packages/,/The following NEW/p }' | sed '/The following NEW/d' | sed '/Suggested /d' )
 # #echo ${GETPACK[@]}
 
 
 # 		 # 	for sendto in "${GETPACK[@]}"; do
-# 		 # 		# echo $sendto 
+# 		 # 		# echo $sendto
 # 			# 	suggestcoll=( $suggestcoll $sendto )
 # 		 # 		# echo $sendto >> "~/.falcon/setup/apt-get/$( hostname -s)_suggested.list"
 # 			# done
