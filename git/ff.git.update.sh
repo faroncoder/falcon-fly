b42865f41345b516ea9b7a46a2c2c55ff412a826
faroncoder <faronledger@gmail.com>
2016-06-26 03:28:47 -0400

    Sun Jun 26 03:28:47 EDT 2016

--- git/ff.git.update.sh
+++ git/ff.git.update.sh
@@ -59,7 +59,7 @@ echo -e "$Finfo updating local git from remote"
 	git add -A
 	git commit -a -m "`date` "
 	git push --set-upstream origin master 2> /dev/null
-	git push -u origin master 1> /dev/null
+	#git push -u origin master 1> /dev/null
 	echo -e "$Fok git updated"
 	sleep 1
 
