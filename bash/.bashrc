452eaca4f4e9210f6ced6046f32e9ab64307eae3
faroncoder <faronledger@gmail.com>
2016-06-27 19:31:54 -0400

    Mon Jun 27 19:31:54 EDT 2016

--- bash/.bashrc
+++ bash/.bashrc
@@ -5,4 +5,4 @@ source $PATHDIR/alias
 source $PATHDIR/environments
 source $PATHDIR/functions
 
-source ~/.bashrc_sources
+#source ~/.bashrc_sources
--- bin/ff.script.new.sh
+++ bin/ff.script.new.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 RETURN=$PWD
 if [ ! "$( echo $PATH | grep '/usr/local/bin' )" ]; then export PATH=$PATH:/usr/local/bin; fi
-source /usr/local/lib/faron_falcon/colors; source /usr/local/lib/faron_falcon/functions;
-if [[ "$1" != "" ]]; then
+source /usr/local/lib/faron_falcon/colors; source /usr/local/lib/faron_falcon/functions; loadSudo; loadEditor;
+#if [[ "$1" != "" ]]; then
 #################### BEGIN
-
-
 newfile="$1"
+newfolder="$2"
 arbin="/mnt/falcon/archives"
-if [ -z "$1" ]; then
+if [[ "$1" == "" ]]; then
 		echo -e -n "$Fstat name of script ? ";
 		read newfile;
 	else
 		echo -e "$Fno name of script required.";
-		logger "`basename $0` : DEBUG: Error 1 - name of script required"
+		logger "FALCON --> $( basename $0 ): DEBUG: Error 1 - name of script required"
 		exit 1;
 fi
-if [ -z "$2" ]; then
+if [[ "$2" == "" ]]; then
 		echo -e -n "$Fstat which folder? ";
 		read newfolder;
 	else
 		echo -e "$Fno name of folder required.";
-		logger "`basename $0` : DEBUG: Error 2 - name of folder required"
+		logger "FALCON --> $( basename $0 ): DEBUG: Error 2 - name of folder required"
 		exit 2;
 fi
 newbin="/mnt/falcon/scripts/$newfolder"
@@ -35,8 +34,8 @@ touch "$newbin/$proofreadfile"
 chmod +x "$newbin/$proofreadfile"
 echo "#!/bin/bash
 if [[ ! \"\$( echo \$PATH | grep '/usr/local/bin' )\" ]]; then export PATH=\$PATH:/usr/local/bin; fi
-source /usr/local/lib/faron_falcon/colors; source /usr/local/lib/faron_falcon/functions;
-RETURN=\$PWD
+source /usr/local/lib/faron_falcon/colors; source /usr/local/lib/faron_falcon/functions;  loadSudo;
+RETURN=\$PWD;
 if [[ \"\$1\" != \"\" ]]; then
 #################### BEGIN
 ## avail for coding in colors: ##
@@ -53,15 +52,10 @@ XeF
 ### IGNORE BELOW. THIS IS MEGATAG FOR MY SCRIPTS
 ### [FALCON] name=\$( basename \$0 ) active=y
 "  >> "$newbin/$proofreadfile"
-echo -e "$Fstat $Fgreen $proofreadfile $Foff created."
-if [[ "$( hostname -s )" == "f10" ]]; then
-	EDITOR="subl"
-else
-	EDITOR="nano"
-fi
-$EDITOR "$newbin/$proofreadfile" 2> /dev/null
+echo -e "$Fok $Fteal $proofreadfile $Foff created."
+$EDITOR "$newbin/$proofreadfile" 2> /dev/null < /dev/null
 ################### END
-else echo -e "$Fwarn Arg 1:$Fyellow name of new script. $Foff \n$Fwarn Arg 2: $Fyellow name of bin you want to put script in $Foff"; exit 1; fi
+#else echo -e "$Fwarn Arg 1:$Fyellow name of new script. $Foff \n$Fwarn Arg 2: $Fyellow name of bin you want to put script in $Foff"; exit 1; fi
 ### exit code for clean exit
 XeF
 ### IGNORE BELOW. THIS IS MEGATAG FOR MY SCRIPTS
