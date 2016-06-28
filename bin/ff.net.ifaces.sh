66cc116f6130d7cb5bbc1df44ddedee852cf9383
faroncoder <faronledger@gmail.com>
2016-06-28 16:31:54 -0400

    Tue Jun 28 16:31:54 EDT 2016

---
+++ bin/ff.net.ifaces.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+if [[ ! "$( echo $PATH | grep '/usr/local/bin' )" ]]; then export PATH=$PATH:/usr/local/bin; fi
+source /usr/local/lib/faron_falcon/colors; source /usr/local/lib/faron_falcon/functions;  loadSudo;
+RETURN=$PWD;
+#if [[ "$1" != "" ]]; then
+#################### BEGIN
+## avail for coding in colors: ##
+## Fok Fno Finfo Fwarn Fstat ##
+## Fred Fblack Fgreen Fyellow Fblue Fpurple Fteal Fwhite Foff ##
+
+$SUDO ifconfig enp1s5 0.0.0.0
+$SUDO ifconfig enp1s5:1 192.168.2.10/24
+$SUDO ifconfig enp1s5:2 64.140.117.18/29
+$SUDO ifconfig enp1s5:3 192.168.1.10/24
+
+ifconfig
+
+route -n
+
+################### END
+#cd $RETURN 1> /dev/null
+#else echo -e "$Fwarn Arg 1:$Fyellow name of arg $Foff \n$Fwarn Arg 2: $Fyellow name of arg $Foff"; exit 1; fi
+### exit code for clean exit
+XeF
+### IGNORE BELOW. THIS IS MEGATAG FOR MY SCRIPTS
+### [FALCON] name=$( basename $0 ) active=y
+
