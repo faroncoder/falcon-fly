df3edb208db507c27f4bb6b0755a1b6b1f0555f0
faroncoder <faronledger@gmail.com>
2016-08-18 18:58:22 -0400

    tweaking

--- bin/ff.load.sh
+++ bin/ff.load.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 RETURN=$PWD
-if [ ! "$( echo $PATH | grep '/usr/local/bin' )" ]; then export PATH=$PATH:/usr/local/bin; fi
-source /usr/local/lib/faron_falcon/colors; source /usr/local/lib/faron_falcon/functions
+if [[ ! "$( echo $PATH | grep '/usr/local/bin' )" ]]; then export PATH=$PATH:/usr/local/bin; fi
+source /usr/local/lib/faron_falcon/colors; source /usr/local/lib/faron_falcon/functions; loadSudo;
 #if [[ "$1" != "" ]]; then
 #################### BEGIN
 
 LOC="/home/users/faron/.falcon/scripts"; if [ ! -d "$LOC" ]; then  echo -e "$Fno Falcon is not mounted. Exiting"; exit 1;   fi
 
-VAR=$1
 FILEHUNT=( ` find $LOC -type f -name 'ff.*' ! -path '*/.git/*' ` );
 
 echo -e "$Finfo $( echo "${FILEHUNT[@]}" | wc -w )"
@@ -16,10 +15,8 @@ makelink(){
 			FILE=`basename $entry`
 			EXT=$( echo $FILE | sed 's/.sh//g' )
 			cd /usr/local/bin 1> /dev/null
-			rm "ff.*" 2> /dev/null
-#			echo -e "$Finfo Falcon Bin sync-ed."
+			/bin/rm "ff.*" 2> /dev/null
 			ln -s "$entry" "$EXT" 2>/dev/null
-# 			echo -e "$Fok $FILE linked";
 }
 
 if [[ -z "$FILEHUNT" ]];  then
@@ -29,28 +26,14 @@ else
 	for entry in "${FILEHUNT[@]}"; do
 		makelink;
 	done
-	# 	SELECT=""
-	# 		while [[ "$SELECT" != $"\x0a" && "$SELECT" != $"\x20" ]]; do
-	# 			echo "${FILEHUNT[@]}"
-	# 			echo -e "$Finfo Proceed?  [g] to proceed"
- #    				IFS= read -s -n 1 SELECT
-	# 			 	case "$SELECT" in
-	# 					g ) makelink ; break;;
-	# 					* ) echo "exiting unchanged"; break;;
-	# 				esac
-	# 		done
-	 # done
 fi
 source ~/.profile
 echo -e "$Fok Falcon Bin sync-ed."
 
-#echo -e $Fok"$Fyellow $( basename $0 ) $Foff"
-
 ################### END
-cd $RETURN 1> /dev/null
-#else echo -e $Finfo "Arg 1=$Fyellow empty $Foff "; fi
+#cd $RETURN 1> /dev/null;
+#else echo -e "$Fstat $Fred Arg 1 $Foff=$Fyellow explain argments before calling. $Foff"; fi
 ### exit code for clean exit
 XeF
 ### IGNORE BELOW. THIS IS MEGATAG FOR MY SCRIPTS
-### [FALCON] name=$( basename $0 ) active=y
-
+### [FILE] /home/users/faron/.falcon/scripts/bin/ff.load.sh  [ACTIVE] y
---
+++ network/ff.ip.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+RETURN=$PWD
+if [[ ! "$( echo $PATH | grep '/usr/local/bin' )" ]]; then export PATH=$PATH:/usr/local/bin; fi
+source /usr/local/lib/faron_falcon/colors; source /usr/local/lib/faron_falcon/functions; loadSudo;
+#if [[ "$1" != "" ]]; then
+#################### BEGIN
+
+
+THIS=`curl http://myip.dnsomatic.com 2> /dev/null`
+echo -e "$Finfo Public IP: $THIS"
+echo -e "$Finfo Local IP: $( hostname -i )"
+echo -e "$Finfo Networked IP: $( hostname -I )"
+
+
+
+################### END
+#cd $RETURN 1> /dev/null;
+#else echo -e "$Fstat $Fred Arg 1 $Foff=$Fyellow explain argments before calling. $Foff"; fi
+### exit code for clean exit
+XeF
+### IGNORE BELOW. THIS IS MEGATAG FOR MY SCRIPTS
+### [FILE] $0  [ACTIVE] y
+
+
